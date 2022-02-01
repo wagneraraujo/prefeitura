49ac8f7a9f79d3b2b3c6f148b52cb8bf33a577a7
Wagner Araújo <wagneraraujo15@gmail.com>
2022-02-01 16:12:49 -0300

    paddig bottom noticias

--- components/ContentCarrousel/index.tsx
+++ components/ContentCarrousel/index.tsx
@@ -1,5 +1,5 @@
 import CarrouselNoticias from '../CarrouselNoticias'
-
+import { ItemNoticia } from '../ItemNoticia'
 export const ContentCarrousel = () => {
   return (
     <div>
@@ -8,11 +8,36 @@ export const ContentCarrousel = () => {
           <h4 className="titleSecoes">Principais Noticias</h4>
         </div>
         <div className="row">
-          <div className="col-md-8 col-xs-12 no-gutter">
+          <div className="col-md-7 col-xs-12 col-lg-7 no-gutter">
             <CarrouselNoticias />
           </div>
 
-          <div className="col-md-4 col-xs-12"></div>
+          <div className="col-md-5 col-xs-12 col-lg-5 colNoticiaWideCarrousel">
+            <ItemNoticia
+              key={1}
+              categoria="Saúde"
+              titulo="Justiça mantém liminar e condena Amazonas Energia por cobrança e retenção indevida da Cosip"
+              imagem="/n1.jpg"
+            />
+            <ItemNoticia
+              key={1}
+              categoria="Saúde"
+              titulo="Justiça mantém liminar e condena Amazonas Energia por cobrança e retenção indevida da Cosip"
+              imagem="/n1.jpg"
+            />
+            <ItemNoticia
+              key={1}
+              categoria="Saúde"
+              titulo="Justiça mantém liminar e condena Amazonas Energia por cobrança e retenção indevida da Cosip"
+              imagem="/n1.jpg"
+            />
+            <ItemNoticia
+              key={1}
+              categoria="Saúde"
+              titulo="Justiça mantém liminar e condena Amazonas Energia por cobrança e retenção indevida da Cosip"
+              imagem="/n1.jpg"
+            />
+          </div>
         </div>
       </div>
     </div>
